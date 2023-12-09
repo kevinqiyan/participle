e5367348f4cfb48659361b8335213782bbbf6756
kevin <kevin1025253@gmail.com>
2023-12-10 00:27:50 +0800

    add:新增utools工具配置

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
   ],
@@ -12,5 +13,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 8080,
+    host: true
   }
 })
